4f68fb84a0eb25b187d147eab2c0d841bf265911
Felipe Diaz <lfeliped@amazon.com>
2020-10-01 16:43:00 -0500

    Add script to clean containers

---
+++ remove.sh
@@ -0,0 +1,11 @@
+# stop all running containers
+echo '####################################################'
+echo 'Stopping running containers (if available)...'
+echo '####################################################'
+docker stop $(docker ps -aq)
+
+# remove all stopped containers
+echo '####################################################'
+echo 'Removing containers ..'
+echo '####################################################'
+docker rm $(docker ps -aq)
